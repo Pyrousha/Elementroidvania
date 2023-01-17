71d9538309cb5cc5dff81b042eed72690c6ef93d
Pyrousha <47787636+Pyrousha@users.noreply.github.com>
2023-01-17 11:51:54 -0600

    Drill dash

--- Assets/_Assets/Scripts/PlayerController.cs
+++ Assets/_Assets/Scripts/PlayerController.cs
@@ -9,12 +9,25 @@ public class PlayerController : Singleton<PlayerController>
     [SerializeField] private BoxCollider2D col;
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Animator anim;
-    private string currentState;
+    private PlayerAnimStateEnum currentAnimation;
 
     //Animation states
-    const string PLAYER_IDLE = "Player_Idle";
-    const string PLAYER_JUMP_UP = "Player_Jump_Up";
-    const string PLAYER_JUMP_DOWN = "Player_Jump_Down";
+    enum PlayerAnimStateEnum
+    {
+        Player_Idle,
+        Player_Jump_Up,
+        Player_Jump_Down,
+        Player_Drill_Right,
+        Player_Drill_Down
+    }
+
+    enum DrillStateEnum
+    {
+        Charged,
+        Drilling,
+        Cooldown
+    }
+    private DrillStateEnum drillState;
 
     [Header("Parameters")]
     [SerializeField] private float accelSpeed;
@@ -26,15 +39,30 @@ public class PlayerController : Singleton<PlayerController>
     [SerializeField] private float gravDown;
     [SerializeField] private float spaceReleaseGravMult;
     [Space(5)]
+    [SerializeField] private float drillPower;
+    [SerializeField] private float drillDuration;
+    [Space(5)]
     [SerializeField] private LayerMask terrainLayer;
 
     private float xSpeed = 0;
     private bool grounded = false;
+    private bool useGravity = true;
     private float lastTimePressedJump = -100.0f;
     private float lastTimeGrounded = -100.0f;
-    private readonly float epsilon = 0.05f;
-    private readonly float jumpBuffer = 0.1f;
-    private readonly float coyoteTime = 0.1f;
+    private float drillTimer;
+    private const float epsilon = 0.05f;
+    private const float jumpBuffer = 0.1f;
+    private const float coyoteTime = 0.1f;
+
+
+    /// <summary>
+    /// Returns if the player is currently able to move (not attacking, dashing, stunned, etc.)
+    /// </summary>
+    /// <returns></returns>
+    private bool CanMove =>
+    (
+        drillState != DrillStateEnum.Drilling
+    );
 
     // Start is called before the first frame update
     void Start()
@@ -45,49 +73,67 @@ public class PlayerController : Singleton<PlayerController>
     // Update is called once per frame
     void Update()
     {
-        //Get sign of current speed
-        float sign = 1;
-        if (xSpeed < 0)
-            sign = -1;
+        if (CanMove)
+        {
+            xSpeed = rb.velocity.x;
 
-        //Accelerate
-        float inputX = InputHandler.Instance.Direction.x;
-        if (inputX < -epsilon) //Pressing left
-        {
-            if (xSpeed > -maxSpeed)
+            //Accelerate + Friction
+            float inputX = InputHandler.Instance.Direction.x;
+            if (inputX < -epsilon) //Pressing left
             {
-                //Can still accelerate to the left (but do not exceed max)
-                xSpeed = Mathf.Max(-maxSpeed, xSpeed + accelSpeed * inputX * Time.deltaTime);
-            }
-        }
-        else
-        {
-            if (inputX > epsilon) //Pressing right
-            {
-                if (xSpeed < maxSpeed)
+                if (xSpeed > -maxSpeed)
                 {
-                    //Can still accelerate to the right (but do not exceed max)
-                    xSpeed = Mathf.Min(maxSpeed, xSpeed + accelSpeed * inputX * Time.deltaTime);
+                    //Can still accelerate to the left (but do not exceed max)
+                    xSpeed = Mathf.Max(-maxSpeed, xSpeed + accelSpeed * inputX * Time.deltaTime);
+                }
+                else
+                {
+                    //Apply friction
+                    xSpeed = Mathf.Min(xSpeed + frictionSpeed * Time.deltaTime, -maxSpeed);
                 }
             }
-            else //pressing nothing
+            else
             {
-                //Not pressing anything, subtract friction and update speed
-                float newSpeedMagnitude = Mathf.Max(0, Mathf.Abs(xSpeed) - frictionSpeed * Time.deltaTime);
-                xSpeed = newSpeedMagnitude * sign;
+                if (inputX > epsilon) //Pressing right
+                {
+                    if (xSpeed < maxSpeed)
+                    {
+                        //Can still accelerate to the right (but do not exceed max)
+                        xSpeed = Mathf.Min(maxSpeed, xSpeed + accelSpeed * inputX * Time.deltaTime);
+                    }
+                    else
+                    {
+                        //Apply friction
+                        xSpeed = Mathf.Max(xSpeed - frictionSpeed * Time.deltaTime, maxSpeed);
+                    }
+                }
+                else //pressing nothing
+                {
+                    //Get sign of current speed
+                    float sign = 1;
+                    if (xSpeed < 0)
+                        sign = -1;
+
+                    //Not pressing anything, subtract friction and update speed
+                    float newSpeedMagnitude = Mathf.Max(0, Mathf.Abs(xSpeed) - frictionSpeed * Time.deltaTime);
+                    xSpeed = newSpeedMagnitude * sign;
+                }
             }
+
+            //Set velocity
+            rb.velocity = new Vector2(xSpeed, rb.velocity.y);
         }
 
-        rb.velocity = new Vector2(xSpeed, rb.velocity.y);
 
         //Sprite flipping
-        if (xSpeed < 0)
+        if (rb.velocity.x < 0)
             spriteRenderer.flipX = true;
         else
         {
-            if (xSpeed > 0)
+            if (rb.velocity.x > 0)
                 spriteRenderer.flipX = false;
         }
+
 
         //Ground checking
         bool lastGrounded = grounded;
@@ -113,30 +159,103 @@ public class PlayerController : Singleton<PlayerController>
         }
 
 
+        #region Change Drill Dash state
+        switch (drillState)
+        {
+            case DrillStateEnum.Charged:
+                {
+                    if (InputHandler.Instance.Drill.down)
+                    {
+                        drillState = DrillStateEnum.Drilling;
+
+                        drillTimer = drillDuration;
+                        useGravity = false;
+
+                        if (InputHandler.Instance.Direction.y < -0.6f)
+                        {
+                            //Drill down
+                            rb.velocity = new Vector2(0, -drillPower);
+
+                            ChangeAnimationState(PlayerAnimStateEnum.Player_Drill_Down);
+                        }
+                        else
+                        {
+                            //Drill horizontal
+                            float inputX = InputHandler.Instance.Direction.x;
+
+                            //holding a direction, dash in that direction
+                            if (Mathf.Abs(inputX) >= epsilon)
+                            {
+                                if (inputX > 0) //Dash right
+                                    rb.velocity = new Vector2(drillPower, 0);
+                                else //Dash left
+                                    rb.velocity = new Vector2(-drillPower, 0);
+                            }
+                            else
+                            {
+                                //not holding a direction, dash in direction player is facing
+                                if (!spriteRenderer.flipX) //dash right
+                                    rb.velocity = new Vector2(drillPower, 0);
+                                else //Dash left
+                                    rb.velocity = new Vector2(-drillPower, 0);
+                            }
+
+                            ChangeAnimationState(PlayerAnimStateEnum.Player_Drill_Right);
+                        }
+                    }
+                    break;
+                }
+            case DrillStateEnum.Drilling:
+                {
+                    drillTimer -= Time.deltaTime;
+                    if (drillTimer <= 0)
+                    {
+                        //End drill
+                        useGravity = true;
+                        drillState = DrillStateEnum.Cooldown;
+                    }
+                    break;
+                }
+            case DrillStateEnum.Cooldown:
+                {
+                    if (grounded)
+                        drillState = DrillStateEnum.Charged;
+                    break;
+                }
+        }
+        #endregion
+
+
+        //Gravity
+        if (useGravity)
+        {
+            if (InputHandler.Instance.Jump.held && rb.velocity.y > 0)
+                rb.velocity -= new Vector2(0, gravUp * Time.deltaTime);
+            else
+                rb.velocity -= new Vector2(0, gravDown * Time.deltaTime);
+        }
+
         //Space release gravity
         if (InputHandler.Instance.Jump.released && rb.velocity.y > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * spaceReleaseGravMult);
         }
 
-        //Gravity
-        if (InputHandler.Instance.Jump.held && rb.velocity.y > 0)
-            rb.velocity -= new Vector2(0, gravUp * Time.deltaTime);
-        else
-            rb.velocity -= new Vector2(0, gravDown * Time.deltaTime);
-
 
         //Set animation states
-        if (grounded)
+        if (drillState != DrillStateEnum.Drilling)
         {
-            ChangeAnimationState(PLAYER_IDLE);
-        }
-        else
-        {
-            if (rb.velocity.y >= 0)
-                ChangeAnimationState(PLAYER_JUMP_UP);
+            if (grounded)
+            {
+                ChangeAnimationState(PlayerAnimStateEnum.Player_Idle);
+            }
             else
-                ChangeAnimationState(PLAYER_JUMP_DOWN);
+            {
+                if (rb.velocity.y >= 0)
+                    ChangeAnimationState(PlayerAnimStateEnum.Player_Jump_Up);
+                else
+                    ChangeAnimationState(PlayerAnimStateEnum.Player_Jump_Down);
+            }
         }
     }
 
@@ -145,16 +264,21 @@ public class PlayerController : Singleton<PlayerController>
         rb.velocity = new Vector2(rb.velocity.x, jumpPower);
     }
 
-    private void ChangeAnimationState(string _newState)
+    private void Drill()
+    {
+
+    }
+
+    private void ChangeAnimationState(PlayerAnimStateEnum _newState)
     {
         //Stop same animation from interrupting itself
-        if (currentState == _newState)
+        if (currentAnimation == _newState)
             return;
 
         //Play new animation
-        anim.Play(_newState);
+        anim.Play(_newState.ToString());
 
         //Update current anim state var
-        currentState = _newState;
+        currentAnimation = _newState;
     }
 }
